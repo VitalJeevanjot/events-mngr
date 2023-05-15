04750d1ab779ce7f01894f4d8782c71dc32d69a3
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2023-05-15 19:31:19 +0530

    2nd commit basic functions declaration

--- html/auto.js
+++ html/auto.js
@@ -1,37 +1,75 @@
-let btn1 = document.getElementById("btn-1");
-console.log(btn1.getBoundingClientRect())
-let box1 = btn1.getBoundingClientRect()
-let x = (box1.left + box1.right) / 2 + 'px'
-let y = (box1.top + box1.bottom) / 2 + 'px'
+let cursor = []
+cursor[0] = document.createElement("img");
 
-let btn2 = document.getElementById("btn-2");
-console.log(btn2.getBoundingClientRect())
-
-const img = document.createElement("img");
-
-img.src = "icons8-cursor-64.png";
-img.style.top = 0;
-img.style.left = 0;
-img.style.zIndex = "9999"; // ensure the image is on top of other elements
-img.style.position = "fixed";
+cursor[0].src = "icons8-cursor-64.png";
+cursor[0].style.top = 0;
+cursor[0].style.left = 0;
+cursor[0].style.zIndex = "9999"; // ensure the image is on top of other elements
+cursor[0].style.position = "fixed";
 
 // add the image to the document body
-document.body.appendChild(img);
+document.body.appendChild(cursor[0]);
 
-img.animate(
-    [
-        // keyframes
-        { transform: "translateY(0px) translateX(0px)" },
-        { transform: `translateY(${y}) translateX(${x})` },
-    ],
-    {
-        // timing options
-        duration: 1000,
-        iterations: 1,
-        fill: "forwards",
-        easing: "ease-out"
+
+
+let _i = 1;
+let _total = 4;
+let start_x = 0;
+let start_y = 0
+function v_DoCursor_Moving()
+{
+    if (_i <= _total) {
+        let btn = document.getElementById("btn-" + _i);
+        console.log(btn.getBoundingClientRect());
+        let box = btn.getBoundingClientRect();
+        let x = (box.left + box.right) / 2;
+        let y = (box.top + box.bottom) / 2;
+
+
+
+
+        let animation = cursor[0].animate(
+            [
+                // keyframes
+                { transform: `translateY(${start_y}px) translateX(${start_x}px)` },
+                { transform: `translateY(${y}px) translateX(${x}px)` },
+            ],
+            {
+                // timing options
+                duration: 800,
+                iterations: 1,
+                fill: "forwards",
+                easing: "cubic-bezier(0.3, 0.4, 0.5, 1.0)"
+            }
+        );
+
+        animation.onfinish = (event) => {
+            console.log("Animation finished");
+            console.log(event);
+            start_x = x;
+            start_y = y;
+            _i++;
+            v_DoCursor_Moving()
+        }
     }
-);
+    
+}
+v_DoCursor_Moving()
+
+
+function v_DoCursor_Clicks() { }
+function v_DoOverlay_Highlights() { }
+function v_DoOverlay_Confirmations() { }
+function v_DoAudio_Playbacks() { }
+function v_ChangeCursor_Style() { }
+
+
+
+
+
+
+
+
 
 
 
@@ -49,3 +87,6 @@ console.log(url)*/
         let src = URL.createObjectURL(blob);
         console.log(src)
     });*/
+
+//let body = document.getElementsByTagName("body");
+//body[0].style. = "none";
